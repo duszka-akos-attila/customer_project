ac6efd4d141d70a9ca9067d1e2209899e6a6771c
Duszka Ákos Attila <duszka.akos.attila@gmail.com>
2020-12-09 14:35:19 +0100

    Wrote the Entity classes for the DAOs.

--- src/main/java/Dao/Entity/Address/AddressEntity.java
+++ src/main/java/Dao/Entity/Address/AddressEntity.java
@@ -1,4 +1,43 @@
 package Dao.Entity.Address;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "address", schema = "sakila")
 public class AddressEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="address_id")
+    private int addressId;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "address2")
+    private String address2;
+
+    @Column(name = "district")
+    private String district;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private CityEntity cityEntity;
+
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "last_update")
+    private Timestamp lastUpdate;
 }
--- src/main/java/Dao/Entity/StoreEntity.java
+++ src/main/java/Dao/Entity/StoreEntity.java
@@ -1,4 +1,33 @@
 package Dao.Entity;
 
+import Dao.Entity.Address.AddressEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "store", schema = "sakila")
 public class StoreEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="store_id")
+    private int storeId;
+
+    @ManyToOne
+    @JoinColumn(name = "staff_id")
+    private StaffEntity managerStaffEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private AddressEntity addressEntity;
+
+    @Column(name = "last_update")
+    private Timestamp lastUpdate;
 }
